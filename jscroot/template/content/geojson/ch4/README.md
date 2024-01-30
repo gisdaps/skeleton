8133b844a52b04c26d945c6a3f818ba0cd14200a
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-01-30 18:30:35 +0700

    update ch4

--- jscroot/template/content/geojson/ch4/README.md
+++ jscroot/template/content/geojson/ch4/README.md
@@ -1,27 +1,7 @@
-# GIS 5
+# GIS4 HTTP POST DATA
 
-Tentu! Berikut adalah contoh README file yang dapat Anda gunakan untuk repository GitHub yang telah Anda sebutkan:
+Post Data Berhasil
+![Alt text](./img/web.PNG)
 
-# GIS-Daffa GitHub Repository
-
-Ini adalah repository untuk proyek GIS yang dikelola oleh Daffa. Repository ini berisi berbagai sumber daya dan proyek terkait sistem informasi geografis (GIS) yang dikembangkan oleh Daffa. Silakan jelajahi berbagai fitur dan sumber daya yang tersedia dalam repository ini.
-
-
-## Deskripsi
-
-Repository ini berisi berbagai komponen dan proyek yang terkait dengan GIS yang dikembangkan oleh Daffa Audya Pramana Untuk Memenuhi Tugas GIS. Silakan temukan proyek-proyek menarik dan sumber daya yang mungkin berguna untuk Anda.
-
-## Link API
-
-```sh
-API LOGIN:
-
-https://us-central1-fahad-402509.cloudfunctions.net/gislogin"
-
-```sh
-Body:
-{
-    "username": "dapa",
-    "password": "dapa"
-}
-
+Data Masuk
+![Alt text](./img/mongo.PNG)
--- jscroot/template/content/geojson/ch4/js/template/template.js
+++ jscroot/template/content/geojson/ch4/js/template/template.js
@@ -1,22 +1,18 @@
-import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
-
-export let URLGeoJson = "https://asia-southeast2-daffaaudya.cloudfunctions.net/gisch3or";
-export let urlPostGCF = "https://asia-southeast2-daffaaudya.cloudfunctions.net/gischp4or";
-export let UrlLogin = "https://asia-southeast2-daffaaudya.cloudfunctions.net/gislogin"
-export let UrlRegister = "https://asia-southeast2-daffaaudya.cloudfunctions.net/gisregister"
+export let URLGeoJson =
+  "https://asia-southeast2-fahad-402509.cloudfunctions.net/gischp3";
+export let urlPOSTGCF =
+  "https://asia-southeast2-fahad-402509.cloudfunctions.net/gischp4";
 export let tableTag = "tr";
 export let tableRowClass = "content is-small";
-export let token = "daffa"
-export let cookie = getCookie("Login")
 export let tableTemplate = `
 <td>#NAME#</td>
 <td >#KORDINAT#</td>
 <td>#TYPE#</td>
-`
+`;
 export const clickpopup = `
 Long : #LONG#<br>
 Lat  : #LAT#<br>
 X   : #X#<br>
 Y   : #Y#<br>
 HDMS : #HDMS#<br>
-`
+`;
