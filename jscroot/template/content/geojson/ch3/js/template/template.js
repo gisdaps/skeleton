69615f13579fbc8dce9abd505942b3cbc8c7895c
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-02-05 18:48:51 +0700

    update url gcf

--- jscroot/template/content/geojson/ch3/js/template/template.js
+++ jscroot/template/content/geojson/ch3/js/template/template.js
@@ -1,4 +1,4 @@
-export let URLGeoJson = "https://asia-southeast2-daffaaudya.cloudfunctions.net/gisch3or";
+export let URLGeoJson = "https://asia-southeast2-daffaaudya.cloudfunctions.net/gis-gcf";
 export let tableTag = "tr";
 export let tableRowClass = "content is-small";
 export let tableTemplate = `
